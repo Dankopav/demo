85fe7e6f951219178282af9d806331041d7964eb
Danko Danko <dankopav@gmail.com>
2017-04-28 00:00:37 +0200

    Adding files

---
+++ src/main/java/hr/danko/controller/HomeController.java
@@ -0,0 +1,17 @@
+package hr.danko.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by danko on 4/27/2017.
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Home";
+    }
+}
+
