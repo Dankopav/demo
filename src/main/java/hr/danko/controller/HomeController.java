91e62b760d2f64e81ca128cde7acf53ad7dcc424
dankopav <dankopav@gmail.com>
2017-05-02 22:08:59 +0200

    igranje REST

--- src/main/java/hr/danko/controller/HomeController.java
+++ src/main/java/hr/danko/controller/HomeController.java
@@ -3,8 +3,7 @@ package hr.danko.controller;
 import hr.danko.model.Auto;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
@@ -16,24 +15,45 @@ public class HomeController {
 
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(HomeController.class);
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
     public String home(){
         log.info("home called");
         return "Home";
     }
 
-    @RequestMapping("/new")
+    @RequestMapping(value="/new", method = RequestMethod.GET)
+    @ResponseBody
         public String getNew(){
             return "New";
         }
 
 
-   @RequestMapping("/getAuto")
+   @RequestMapping(value="/getAuto", method=RequestMethod.GET)
+   @ResponseBody
         public Auto getAuto(){
             Auto auto=new Auto();
             auto.setId(1);
             auto.setName("Golf");
             return auto;
         }
+
+    @RequestMapping(value="/math", method=RequestMethod.GET)
+    @ResponseBody
+    public String calculation(){
+            return "Math pozvan";
+    }
+    @RequestMapping(value="/math/zbroj", method = RequestMethod.GET,produces={"application/json"})
+    @ResponseBody
+        public String suma(@RequestParam("broj") int broj){
+            return "Unili ste broj:"+broj;
+        }
+
+    @RequestMapping("*")
+    @ResponseBody
+    public String fallbackMethod(){
+        return "fallback method";
+    }
+
 }
 
